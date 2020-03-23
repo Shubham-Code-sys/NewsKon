3849738ba8c7801a6ed246093a3a9f2279500b5e
Shubham Gupta <praneetgupta009@gmail.com>
2020-03-23 13:46:26 +0530

    almost complete

--- javascript.js
+++ javascript.js
@@ -9,3 +9,32 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+/* image preview */
+
+const inpFile = document.getElementById("inpfile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+inpFile.addEventListener("change", function() {
+    const file = this.files[0];
+
+    if(file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function() {
+            console.log(this);
+            previewImage.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    } else{
+        previewDefaultText.style.display = null;
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "");
+    }
+});
